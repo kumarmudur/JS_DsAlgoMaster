8458109d8ff36a70e2e2d80e9f61cc31c565b4a4
Basavakumar10 <basavakumar.mudur@indecomm.com>
2019-12-17 11:26:15 +0530

    frequency counter example

---
+++ patterns/frequency.js
@@ -0,0 +1,44 @@
+// naive solution
+
+function same(arr1, arr2) {
+    if(arr1.length !== arr2.length) {
+        return false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+        let correctIndex = arr2.indexOf(arr1[i] ** 2);
+        if(correctIndex === -1) {
+            return false;
+        }
+        arr2.splice(correctIndex, 1);
+    }
+    return true;
+}
+
+// refactor code
+function same(arr1, arr2) {
+    if(arr1.length !== arr2.length) {
+        return false;
+    }
+    let frequencyCounter1 = {};
+    let frequencyCounter2 = {};
+    for (let val of arr1) {
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+        console.log(frequencyCounter1);
+    }
+    for (let val of arr2) {
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+        console.log(frequencyCounter2);
+    }
+
+    for (let key in frequencyCounter1) {
+        if(!(key ** 2 in frequencyCounter2)) {
+            return false;
+        }
+        if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(same([1, 2, 3, 2], [4, 1, 8, 4 ]));
