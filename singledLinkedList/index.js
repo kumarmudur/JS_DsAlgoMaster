a0a5c62a87e946bde678cfbbdc10670f42ac8259
Basavakumar10 <basavakumar.mudur@indecomm.com>
2019-12-26 18:53:04 +0530

    get and set method

--- singledLinkedList/index.js
+++ singledLinkedList/index.js
@@ -71,6 +71,28 @@ class singlyLinkedList {
         this.length++;
         return this;
     }
+
+    // get
+    get(index) {
+        if (index < 0 || index >= this.length) return null;
+        var counter = 0;
+        var current = this.head;
+        while (counter !== index) {
+            current = current.next;
+            counter++;
+        }
+        return current;
+    }
+
+    // set
+    set(index, val) {
+        var foundNode = this.get(index);
+        if (foundNode) {
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
 }
 
 var list = new singlyLinkedList();
