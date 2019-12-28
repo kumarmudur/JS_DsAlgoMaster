743b367b9d6b42309559f7a04c68c9c8ccccffeb
Basavakumar10 <basavakumar.mudur@indecomm.com>
2019-12-28 19:16:06 +0530

    doubly linked list implementation

--- doublyLinkedList/index.js
+++ doublyLinkedList/index.js
@@ -74,4 +74,72 @@ class DoublyLinkedList {
         this.length++;
         return this;
     }
+
+    // get
+    get(index) {
+        if (index < 0 || index >= this.length) return null;
+        var count, current;
+        if (index <= this.length/2) {
+            count = 0;
+            current = this.head;
+            while(count !== index) {
+                current = current.next;
+                count++;
+            }
+        } else {
+            count = this.length - 1;
+            current = this.tail;
+            while(count !== index) {
+                current = current.prev;
+                count--;
+            }
+        }
+        return current;
+    }
+
+    // set
+    set(index, val) {
+        var foundNode = this.get(index);
+        if(foundNode !== null) {
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
+
+    // insert
+    insert(index, val) {
+        if (index < 0 || index > this.length) return false;
+        if (index === 0) return !!this.unshift(val);
+        if (index === this.length) return !!this.push(val); 
+        var newNode = new Node(val);
+        var beforeNode = this.get(index - 1);
+        var afterNode = beforeNode.next;
+
+        beforeNode.next = newNode;
+        newNode.prev = beforeNode;
+        newNode.next = afterNode;
+        afterNode.prev = newNode;
+        
+        this.length++;
+        return true;
+    }   
+
+    //remove
+    remove(index) {
+        if (index < 0 || index >= this.length) return undefined;
+        if (index === 0) return this.shift();
+        if (index === this.length - 1) return this.pop();
+        var removedNode = this.get(index);
+        var beforeNode = removedNode.prev;
+        var afterNode = removedNode.next;
+        beforeNode.next = afterNode;
+        afterNode.prev = beforeNode;
+        // removedNode.prev.next = removedNode.next;
+        // removedNode.next.prev = removedNode.prev;
+        removedNode.next = null;
+        removedNode.prev = null;
+        this.length--;
+        return removedNode;
+    }
 }
