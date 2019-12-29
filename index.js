298df6a105ff9494bdcb046db93ed48b34bca447
Basavakumar10 <basavakumar.mudur@indecomm.com>
2019-12-29 16:51:25 +0530

    stack implementation

---
+++ index.js
@@ -0,0 +1,40 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    // push
+    push(val) {
+        var newNode = new Node(val);
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            var temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
+    }
+
+    // pop
+    pop() {
+        if(!this.first) return null;
+        var temp = this.first;
+        if (this.first === this.last) {
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.val;
+    }
+}
